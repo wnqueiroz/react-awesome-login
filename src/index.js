711d2a54e3f44a215bf77b7bb83ff4669aaf0fea
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-09-27 21:31:46 -0300

    Created the first static page version

--- src/index.js
+++ src/index.js
@@ -1,9 +1,58 @@
-import React, {Component} from 'react'
+import React, { Component } from "react"
 
-export default class extends Component {
+import "./styles.scss"
+
+import TextField from "@material-ui/core/TextField"
+
+class AwesomeLogin extends Component {
   render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+    return (
+      <div className="login-page">
+        <aside className="aside-left">
+          <nav className="navbar">
+            <img
+              src="http://pigment.github.io/fake-logos/logos/small/color/the-dance-studio.png"
+              class="brand"
+            />
+
+            <ul className="list">
+              <li className="item -active">Sign In</li>
+              <li className="item">Sign Up</li>
+            </ul>
+          </nav>
+
+          <form action="" className="login-form">
+            <h1 className="title">Sign In</h1>
+            <h2 className="description">
+              Sign in to continue to our application
+            </h2>
+
+            <div className="input-group">
+              <TextField
+                id="standard-required"
+                label="E-mail"
+                margin="normal"
+                fullWidth
+              />
+              <TextField
+                id="standard-required"
+                label="Password"
+                margin="normal"
+                type="password"
+                fullWidth
+              />
+              <div className="input">
+                <input type="submit" value="Submit" />
+              </div>
+            </div>
+          </form>
+        </aside>
+        <aside className="content">
+          <span className="welcome-message">Welcome to Your Company</span>
+        </aside>
+      </div>
+    )
   }
 }
+
+export default AwesomeLogin
