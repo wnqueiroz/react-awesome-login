d21c3fefc174f8013b7e88a5377ab3327172b515
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-09-27 21:25:18 -0300

    Configuring nwb to compile sass

--- nwb.config.js
+++ nwb.config.js
@@ -1,7 +1,16 @@
+const path = require("path")
+
 module.exports = {
-  type: 'react-component',
+  type: "react-component",
   npm: {
     esModules: true,
     umd: false
+  },
+  webpack: {
+    rules: {
+      sass: {
+        includePaths: [path.resolve("src/styles")]
+      }
+    }
   }
 }
